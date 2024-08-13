b68c284be50f29670b1f079c5f8314a47e9d9842
Vanessa lins <69607861+Vanessa867@users.noreply.github.com>
2024-08-12 22:40:15 -0300

    Add files via upload

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div className="my-component">
+      <p>A bandeira mais bonita do Brasil</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY40jLOXUiCP9ZJYu0Is-KFelz3hNB-6ctUA&s" alt="Bandeira de Pernanbuco" />
+      
+    </div>
+  );
+}
+
+export default MyComponent;
